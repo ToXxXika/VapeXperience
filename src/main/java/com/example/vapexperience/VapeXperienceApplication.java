c4c327882c623fb24e7a2704ef46d9a3c8d1a592
ToXxXika <mabrouki552@gmail.com>
2023-05-13 19:17:32 -0700

    First Commit

---
+++ src/main/java/com/example/vapexperience/VapeXperienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vapexperience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapeXperienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VapeXperienceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vapexperience/VapeXperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vapexperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VapeXperienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
